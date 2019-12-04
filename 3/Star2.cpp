238574c8913c1645ad15ea0f1b0b94406b9f0332
lnarolski <narolskilukasz+github@gmail.com>
2019-12-04 20:21:11 +0100

    Day 3

---
+++ 3/Star2.cpp
@@ -0,0 +1,515 @@
+#define FIRST_WIRE 1
+#define SECOND_WIRE 2
+#define INTERSECTION 3
+#define NOT_WIRE 0
+
+#include <iostream>
+#include <vector>
+#include <ctype.h>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+enum moveType
+{
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+};
+
+struct intersection
+{
+    long posX;
+    long posY;
+    long stepsToIntersectionFirstWire;
+    long stepsToIntersectionSecondWire;
+    long sumOfSteps;
+};
+
+long calculateManhattanDistance(long x1, long y1, long x2, long y2)
+{
+    return abs(x2 - x1) + abs(y2 - y1);
+}
+
+void addStepsToVector(long posX, long posY, vector <intersection*> intersections, int numOfWire, long numOfSteps)
+{
+    for (int i = 0; i < intersections.size(); ++i)
+    {
+        if (intersections[i]->posX == posX && intersections[i]->posY == posY)
+        {
+            if (numOfWire == 0)
+                intersections[i]->stepsToIntersectionFirstWire = numOfSteps;
+            else
+                intersections[i]->stepsToIntersectionSecondWire = numOfSteps;
+            return;
+        }
+    }
+}
+
+long findMinimumNumberOfSteps(vector <intersection*> intersections)
+{
+    long minimumNumberOfSteps = LONG_MAX;
+    for (int i = 0; i < intersections.size(); ++i)
+    {
+        intersections[i]->sumOfSteps = intersections[i]->stepsToIntersectionFirstWire + intersections[i]->stepsToIntersectionSecondWire;
+        if (intersections[i]->sumOfSteps < minimumNumberOfSteps)
+            minimumNumberOfSteps = intersections[i]->sumOfSteps;
+    }
+
+    return minimumNumberOfSteps;
+}
+
+int main(int argc, char *argv[])
+{
+    string buffer = "";
+
+    fstream input;
+    string fileLine;
+    input.open("input.txt", fstream::in);
+
+    char **grid;
+
+    long minX[3] = {0, 0, 0}, minY[3] = {0, 0, 0}, maxX[3] = {0, 0, 0}, maxY[3] = {0, 0, 0}, posX, posY;
+    moveType moveT;
+
+    int lineNumber = 0;
+
+    // Calculating size of the grid
+    while (getline(input, fileLine))
+    {
+        posX = 0;
+        posY = 0;
+
+        for (int i = 0; i < fileLine.size() + 1; ++i)
+        {
+            if (isalpha(fileLine[i]))
+            {
+                switch (fileLine[i])
+                {
+                case 'U':
+                    moveT = UP;
+                    break;
+                case 'D':
+                    moveT = DOWN;
+                    break;
+                case 'R':
+                    moveT = RIGHT;
+                    break;
+                case 'L':
+                    moveT = LEFT;
+                    break;
+                default:
+                    break;
+                }
+            }
+            else if (isdigit(fileLine[i]))
+            {
+                buffer.push_back(fileLine[i]);
+            }
+            else
+            {
+                switch (moveT)
+                {
+                case UP:
+                    posY += stoi(buffer);
+                    break;
+                case DOWN:
+                    posY -= stoi(buffer);
+                    break;
+                case RIGHT:
+                    posX += stoi(buffer);
+                    break;
+                case LEFT:
+                    posX -= stoi(buffer);
+                    break;
+                default:
+                    break;
+                }
+
+                buffer = "";
+
+                if (posX < minX[lineNumber])
+                {
+                    minX[lineNumber] = posX;
+                }
+                else if (posX > maxX[lineNumber])
+                {
+                    maxX[lineNumber] = posX;
+                }
+                else if (posY < minY[lineNumber])
+                {
+                    minY[lineNumber] = posY;
+                }
+                else if (posY > maxY[lineNumber])
+                {
+                    maxY[lineNumber] = posY;
+                }
+            }
+        }
+        ++lineNumber;
+    }
+
+    input.close();
+
+    minX[2] = minX[0] < minX[1] ? minX[0] : minX[1];
+    minY[2] = minY[0] < minY[1] ? minY[0] : minY[1];
+    maxX[2] = maxX[0] > maxX[1] ? maxX[0] : maxX[1];
+    maxY[2] = maxY[0] > maxY[1] ? maxY[0] : maxY[1];
+
+    long yBIAS = abs(minY[2]);
+    long xBIAS = abs(minX[2]);
+
+    long widthGrid = abs(minX[2]) + maxX[2];
+    long heightGrid = abs(minY[2]) + maxY[2];
+
+    long startX = xBIAS;
+    long startY = yBIAS;
+
+    long minimumDistance = LONG_MAX;
+
+    // Create matrix for grid
+    grid = new char*[widthGrid + 1];
+    for (int i = 0; i < widthGrid + 1; ++i)
+    {
+        grid[i] = new char[heightGrid + 1];
+    }
+
+    for (int i = 0; i < widthGrid + 1; ++i)
+    {
+        for (int j = 0; j < heightGrid + 1; ++j)
+        {
+            grid[i][j] = NOT_WIRE;
+        }
+    }
+
+    // Looking for intersections
+    long numberOfIntersections = 0;
+    vector <intersection*> intersections;
+
+    lineNumber = 0;
+    input.open("input.txt", fstream::in);
+    while (getline(input, fileLine))
+    {
+        posX = startX;
+        posY = startY;
+
+        for (int i = 0; i < fileLine.size() + 1; ++i)
+        {
+            if (isalpha(fileLine[i]))
+            {
+                switch (fileLine[i])
+                {
+                case 'U':
+                    moveT = UP;
+                    break;
+                case 'D':
+                    moveT = DOWN;
+                    break;
+                case 'R':
+                    moveT = RIGHT;
+                    break;
+                case 'L':
+                    moveT = LEFT;
+                    break;
+                default:
+                    break;
+                }
+            }
+            else if (isdigit(fileLine[i]))
+            {
+                buffer.push_back(fileLine[i]);
+            }
+            else
+            {
+                int bufferINT = stoi(buffer);
+                switch (moveT)
+                {
+                case UP:
+                    for (int k = 0; k < stoi(buffer); ++k)
+                    {
+                        posY += 1;
+                        if (grid[posX][posY] == FIRST_WIRE && lineNumber == 1)
+                        {
+                            long distance = calculateManhattanDistance(startX, startY, posX, posY);
+                            if (distance < minimumDistance)
+                                minimumDistance = distance;
+
+                            grid[posX][posY] = INTERSECTION;
+
+                            intersection *temp = new intersection();
+                            temp->posX = posX;
+                            temp->posY = posY;
+                            intersections.push_back(temp);
+
+                            ++numberOfIntersections;
+                        }
+                        else
+                        {
+                            if (lineNumber == 0)
+                            {
+                                grid[posX][posY] = FIRST_WIRE;
+                            }
+                            else
+                            {
+                                grid[posX][posY] = SECOND_WIRE;
+                            }
+                        }
+                    }
+                    break;
+                case DOWN:
+                    for (int k = 0; k < stoi(buffer); ++k)
+                    {
+                        posY -= 1;
+                        if (grid[posX][posY] == FIRST_WIRE && lineNumber == 1)
+                        {
+                            long distance = calculateManhattanDistance(startX, startY, posX, posY);
+                            if (distance < minimumDistance)
+                                minimumDistance = distance;
+
+                            grid[posX][posY] = INTERSECTION;
+
+                            intersection *temp = new intersection();
+                            temp->posX = posX;
+                            temp->posY = posY;
+                            intersections.push_back(temp);
+
+                            ++numberOfIntersections;
+                        }
+                        else
+                        {
+                            if (lineNumber == 0)
+                            {
+                                grid[posX][posY] = FIRST_WIRE;
+                            }
+                            else
+                            {
+                                grid[posX][posY] = SECOND_WIRE;
+                            }
+                        }
+                    }
+                    break;
+                case RIGHT:
+                    for (int k = 0; k < bufferINT; ++k)
+                    {
+                        posX += 1;
+                        if (grid[posX][posY] == FIRST_WIRE && lineNumber == 1)
+                        {
+                            long distance = calculateManhattanDistance(startX, startY, posX, posY);
+                            if (distance < minimumDistance)
+                                minimumDistance = distance;
+
+                            grid[posX][posY] = INTERSECTION;
+
+                            intersection *temp = new intersection();
+                            temp->posX = posX;
+                            temp->posY = posY;
+                            intersections.push_back(temp);
+
+                            ++numberOfIntersections;
+                        }
+                        else
+                        {
+                            if (lineNumber == 0)
+                            {
+                                grid[posX][posY] = FIRST_WIRE;
+                            }
+                            else
+                            {
+                                grid[posX][posY] = SECOND_WIRE;
+                            }
+                        }
+                    }
+                    break;
+                case LEFT:
+                    bufferINT = stoi(buffer);
+                    for (int k = 0; k < bufferINT; ++k)
+                    {
+                        posX -= 1;
+                        if (grid[posX][posY] == FIRST_WIRE && lineNumber == 1)
+                        {
+                            long distance = calculateManhattanDistance(startX, startY, posX, posY);
+                            if (distance < minimumDistance)
+                                minimumDistance = distance;
+
+                            grid[posX][posY] = INTERSECTION;
+
+                            intersection *temp = new intersection();
+                            temp->posX = posX;
+                            temp->posY = posY;
+                            intersections.push_back(temp);
+
+                            ++numberOfIntersections;
+                        }
+                        else
+                        {
+                            if (lineNumber == 0)
+                            {
+                                grid[posX][posY] = FIRST_WIRE;
+                            }
+                            else
+                            {
+                                grid[posX][posY] = SECOND_WIRE;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+                }
+
+                buffer = "";
+            }
+        }
+        ++lineNumber;
+    }
+
+    input.close();
+
+    // Calculating number of steps to every intersection for wire 1 and wire 2
+    lineNumber = 0;
+    input.open("input.txt", fstream::in);
+    while (getline(input, fileLine))
+    {
+        posX = startX;
+        posY = startY;
+
+        long stepsToIntersection = 0;
+
+        for (int i = 0; i < fileLine.size() + 1; ++i)
+        {
+            if (isalpha(fileLine[i]))
+            {
+                switch (fileLine[i])
+                {
+                case 'U':
+                    moveT = UP;
+                    break;
+                case 'D':
+                    moveT = DOWN;
+                    break;
+                case 'R':
+                    moveT = RIGHT;
+                    break;
+                case 'L':
+                    moveT = LEFT;
+                    break;
+                default:
+                    break;
+                }
+            }
+            else if (isdigit(fileLine[i]))
+            {
+                buffer.push_back(fileLine[i]);
+            }
+            else
+            {
+                switch (moveT)
+                {
+                case UP:
+                    for (int k = 0; k < stoi(buffer); ++k)
+                    {
+                        posY += 1;
+                        if (lineNumber == 0)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 0, stepsToIntersection);
+                            }
+                        }
+                        else if (lineNumber == 1)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 1, stepsToIntersection);
+                            }
+                        }
+                    }
+                    break;
+                case DOWN:
+                    for (int k = 0; k < stoi(buffer); ++k)
+                    {
+                        posY -= 1;
+                        if (lineNumber == 0)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 0, stepsToIntersection);
+                            }
+                        }
+                        else if (lineNumber == 1)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 1, stepsToIntersection);
+                            }
+                        }
+                    }
+                    break;
+                case RIGHT:
+                    for (int k = 0; k < stoi(buffer); ++k)
+                    {
+                        posX += 1;
+                        if (lineNumber == 0)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 0, stepsToIntersection);
+                            }
+                        }
+                        else if (lineNumber == 1)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 1, stepsToIntersection);
+                            }
+                        }
+                    }
+                    break;
+                case LEFT:
+                    for (int k = 0; k < stoi(buffer); ++k)
+                    {
+                        posX -= 1;
+                        if (lineNumber == 0)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 0, stepsToIntersection);
+                            }
+                        }
+                        else if (lineNumber == 1)
+                        {
+                            ++stepsToIntersection;
+                            if (grid[posX][posY] == INTERSECTION)
+                            {
+                                addStepsToVector(posX, posY, intersections, 1, stepsToIntersection);
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+                }
+
+                buffer = "";
+            }
+        }
+        ++lineNumber;
+    }
+
+    input.close();
+
+    cout << "Number of steps: " << findMinimumNumberOfSteps(intersections) << endl;
+
+}
