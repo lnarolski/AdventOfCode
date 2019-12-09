55c65964abca67604ae1551c8863782ab9cae72a
lnarolski <narolskilukasz+github@gmail.com>
2019-12-09 21:13:15 +0100

    Day 9

---
+++ 9/Star1/Star1/Star1.cpp
@@ -0,0 +1,289 @@
+#define ADD_COMM 1
+#define MUL_COMM 2
+#define INPUT_COMM 3
+#define OUTPUT_COMM 4
+#define JUMP_IF_TRUE 5
+#define JUMP_IF_FALSE 6
+#define LESS_THAN 7
+#define EQUALS 8
+#define ADJ_RELATIVE_BASE 9
+#define END_COMM 99
+
+#include <iostream>
+#include <vector>
+#include <ctype.h>
+#include <string>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+void expandMemory(vector <long long>* commands, long j)
+{
+	long long size = commands->size();
+	for (long long i = 0; i < j - size + 1; ++i)
+	{
+		commands->push_back(0);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	string buffer = "";
+	long long inputValue = 1;
+	long long outputValue = 0;
+	long long numberOfCommands = 0;
+
+	vector <long long> commands;
+
+	fstream input;
+	string fileLine;
+	input.open("input.txt", fstream::in);
+
+	while (getline(input, fileLine))
+	{
+		for (int i = 0; i < fileLine.size() + 1; ++i)
+		{
+			if (fileLine[i] == '-')
+			{
+				buffer.push_back(fileLine[i]);
+			}
+			else if (isdigit(fileLine[i]))
+			{
+				buffer.push_back(fileLine[i]);
+			}
+			else
+			{
+				commands.push_back(stoll(buffer));
+				buffer = "";
+				++numberOfCommands;
+			}
+		}
+	}
+
+	input.close();
+
+	long j = 0;
+	long relativeBase = 0;
+
+	int command = commands[j] % 100;
+	int firstParameterMode = (commands[j] % 1000) / 100;
+	int secondParameterMode = (commands[j] % 10000) / 1000;
+	int thirdParameterMode = (commands[j] % 100000) / 10000;
+
+	if (firstParameterMode == 0 && commands[j + 1] > (commands.size() - 1))
+	{
+		expandMemory(&commands, commands[j + 1]);
+	}
+	else if (firstParameterMode == 2 && (relativeBase + commands[j + 1]) > commands.size() - 1)
+	{
+		expandMemory(&commands, relativeBase + commands[j + 1]);
+	}
+	else if (firstParameterMode == 1 && j + 1 > commands.size() - 1)
+	{
+		expandMemory(&commands, j + 1);
+	}
+
+	if (secondParameterMode == 0 && commands[j + 2] > (commands.size() - 1))
+	{
+		expandMemory(&commands, commands[j + 2]);
+	}
+	else if (secondParameterMode == 2 && (relativeBase + commands[j + 2]) > commands.size() - 1)
+	{
+		expandMemory(&commands, relativeBase + commands[j + 2]);
+	}
+	else if (secondParameterMode == 1 && j + 2 > commands.size() - 1)
+	{
+		expandMemory(&commands, j + 1);
+	}
+
+	if (thirdParameterMode == 0 && commands[j + 3] > (commands.size() - 1))
+	{
+		expandMemory(&commands, commands[j + 3]);
+	}
+	else if (thirdParameterMode == 2 && (relativeBase + commands[j + 3]) > commands.size() - 1)
+	{
+		expandMemory(&commands, relativeBase + commands[j + 3]);
+	}
+	else if (thirdParameterMode == 1 && j + 3 > commands.size() - 1)
+	{
+		expandMemory(&commands, j + 3);
+	}
+
+	while (command != END_COMM)
+	{
+		switch (command)
+		{
+		case ADD_COMM:
+			if (thirdParameterMode == 0)
+			{
+				commands[commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) + (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == commands[j + 3]))
+					j = j + 4;
+			}
+			else //thirdParameterMode == 2
+			{
+				commands[relativeBase + commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) + (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == relativeBase + commands[j + 3]))
+					j = j + 4;
+			}
+			break;
+		case MUL_COMM:
+			if (thirdParameterMode == 0)
+			{
+				commands[commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) * (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == commands[j + 3]))
+					j = j + 4;
+			}
+			else //thirdParameterMode == 2
+			{
+				commands[relativeBase + commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) * (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == relativeBase + commands[j + 3]))
+					j = j + 4;
+			}
+			break;
+		case INPUT_COMM:
+			(firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) = inputValue;
+			if (!(j == commands[j + 3]))
+				j = j + 2;
+			break;
+		case OUTPUT_COMM:
+			outputValue = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]);
+			cout << "Output: " << outputValue << endl;
+			j = j + 2;
+			break;
+		case JUMP_IF_TRUE:
+			if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) != 0)
+			{
+				j = (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+			}
+			else
+				j = j + 3;
+			break;
+		case JUMP_IF_FALSE:
+			if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) == 0)
+			{
+				j = (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+			}
+			else
+				j = j + 3;
+			break;
+		case LESS_THAN:
+			if (thirdParameterMode == 0)
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) < (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[commands[j + 3]] = 1;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[commands[j + 3]] = 0;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			else //thirdParameterMode == 2
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) < (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[relativeBase + commands[j + 3]] = 1;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[relativeBase + commands[j + 3]] = 0;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			break;
+		case EQUALS:
+			if (thirdParameterMode == 0)
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) == (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[commands[j + 3]] = 1;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[commands[j + 3]] = 0;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			else //thirdParameterMode == 2
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) == (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[relativeBase + commands[j + 3]] = 1;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[relativeBase + commands[j + 3]] = 0;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			break;
+		case ADJ_RELATIVE_BASE:
+			relativeBase += (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]);
+			j = j + 2;
+			break;
+		default:
+			cout << "UndefOpcode: " << command << endl;
+			++j;
+			break;
+		}
+
+		command = commands[j] % 100;
+		firstParameterMode = (commands[j] % 1000) / 100;
+		secondParameterMode = (commands[j] % 10000) / 1000;
+		thirdParameterMode = (commands[j] % 100000) / 10000;
+
+		if (firstParameterMode == 0 && commands[j + 1] > (commands.size() - 1))
+		{
+			expandMemory(&commands, commands[j + 1]);
+		}
+		else if (firstParameterMode == 2 && (relativeBase + commands[j + 1]) > commands.size() - 1)
+		{
+			expandMemory(&commands, relativeBase + commands[j + 1]);
+		}
+		else if (firstParameterMode == 1 && j + 1 > commands.size() - 1)
+		{
+			expandMemory(&commands, j + 1);
+		}
+
+		if (secondParameterMode == 0 && commands[j + 2] > (commands.size() - 1))
+		{
+			expandMemory(&commands, commands[j + 2]);
+		}
+		else if (secondParameterMode == 2 && (relativeBase + commands[j + 2]) > commands.size() - 1)
+		{
+			expandMemory(&commands, relativeBase + commands[j + 2]);
+		}
+		else if (secondParameterMode == 1 && j + 2 > commands.size() - 1)
+		{
+			expandMemory(&commands, j + 1);
+		}
+
+		if (thirdParameterMode == 0 && commands[j + 3] > (commands.size() - 1))
+		{
+			expandMemory(&commands, commands[j + 3]);
+		}
+		else if (thirdParameterMode == 2 && (relativeBase + commands[j + 3]) > commands.size() - 1)
+		{
+			expandMemory(&commands, relativeBase + commands[j + 3]);
+		}
+		else if (thirdParameterMode == 1 && j + 3 > commands.size() - 1)
+		{
+			expandMemory(&commands, j + 3);
+		}
+	}
+}
