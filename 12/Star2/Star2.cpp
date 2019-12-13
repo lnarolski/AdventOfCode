019ee9b9e03a9e3dd93be946b849e3cf22ece459
Łukasz Narolski <narolskilukasz+github@gmail.com>
2019-12-13 19:28:36 +0100

    Day 12, Day 13

--- 12/Star2/Star2.cpp
+++ 12/Star2/Star2.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <numeric>
 
 int main()
 {
@@ -12,7 +12,9 @@ int main()
 	input.open("input.txt", std::fstream::in);
 
 	std::vector <std::vector <long>> coordinatesMoons;
+	std::vector <std::vector <long>> initialCoordinatesMoons;
 	std::vector <std::vector <long>> speedMoons;
+	std::vector <unsigned long long> numberOfSteps;
 
 	while (getline(input, fileLine))
 	{
@@ -53,10 +55,25 @@ int main()
 		speeds.push_back(0);
 
 		speedMoons.push_back(speeds);
+
+		std::vector <long> initialCoordinates;
+
+		initialCoordinates.push_back(coordinatesMoons[i][0]);
+		initialCoordinates.push_back(coordinatesMoons[i][1]);
+		initialCoordinates.push_back(coordinatesMoons[i][2]);
+
+		initialCoordinatesMoons.push_back(initialCoordinates);
 	}
 
-	int steps = 0;
-	while (steps < 4686774924)
+	for (int i = 0; i < 3; ++i)
+	{
+		numberOfSteps.push_back(0);
+	}
+
+	unsigned long long steps = 0;
+	bool stop = false;
+
+	while (!stop)
 	{
 		for (int i = 0; i < coordinatesMoons.size(); ++i)
 		{
@@ -86,28 +103,27 @@ int main()
 		}
 
 		++steps;
+
+		for (size_t i = 0; i < 3; ++i)
+		{
+			if (initialCoordinatesMoons[0][i] == coordinatesMoons[0][i] && initialCoordinatesMoons[1][i] == coordinatesMoons[1][i] && initialCoordinatesMoons[2][i] == coordinatesMoons[2][i] && initialCoordinatesMoons[3][i] == coordinatesMoons[3][i] && numberOfSteps[i] == 0)
+			{
+				numberOfSteps[i] = steps + 1;
+			}
+		}
+
+		stop = true;
+		for (size_t i = 0; i < numberOfSteps.size(); ++i)
+		{
+			if (numberOfSteps[i] == 0)
+			{
+				stop = false;
+				break;
+			}
+		}
 	}
 
-	std::vector <std::vector <long>> energy;
+	unsigned long long stepsPreviousState = std::lcm(numberOfSteps[0], std::lcm(numberOfSteps[1], numberOfSteps[2]));
 
-	for (int i = 0; i < coordinatesMoons.size(); ++i)
-	{
-		std::vector <long> energyTemp;
-
-		energyTemp.push_back(abs(coordinatesMoons[i][0]) + abs(coordinatesMoons[i][1]) + abs(coordinatesMoons[i][2]));
-
-		energyTemp.push_back(abs(speedMoons[i][0]) + abs(speedMoons[i][1]) + abs(speedMoons[i][2]));
-
-		energyTemp.push_back(energyTemp[0] * energyTemp[1]);
-
-		energy.push_back(energyTemp);
-	}
-
-	long energyTotal = 0;
-	for (int i = 0; i < coordinatesMoons.size(); ++i)
-	{
-		energyTotal += energy[i][2];
-	}
-
-	std::cout << "Total energy after 1000 steps: " << energyTotal << std::endl;
+	std::cout << "Number of steps: " << stepsPreviousState << std::endl;
 }
---
+++ 13/Star1/main.cpp
@@ -0,0 +1,371 @@
+#define ADD_COMM 1
+#define MUL_COMM 2
+#define INPUT_COMM 3
+#define OUTPUT_COMM 4
+#define JUMP_IF_TRUE 5
+#define JUMP_IF_FALSE 6
+#define LESS_THAN 7
+#define EQUALS 8
+#define ADJ_RELATIVE_BASE 9
+#define END_COMM 99
+
+#define EMPTY 0
+#define WALL 1
+#define BLOCK 2
+#define HORIZONTAL_PADDLE 3
+#define BALL 4
+#define NULL_VALUE -1
+
+#include <stdio.h>
+#include <iostream>
+#include <ctype.h>
+#include <string.h>
+#include <fstream>
+#include <vector>
+#include <string>
+
+void expandMemory(std::vector <long long>* commands, long j)
+{
+	long long size = commands->size();
+	for (long long i = 0; i < j - size + 1; ++i)
+	{
+		commands->push_back(0);
+	}
+}
+
+long j = 0;
+long relativeBase = 0;
+
+std::vector <long long> commands;
+long long inputValue = 0;
+std::vector <long long> outputValues;
+bool computerStop = false;
+
+void IntCodeComputer()
+{
+	int command = commands[j] % 100;
+	int firstParameterMode = (commands[j] % 1000) / 100;
+	int secondParameterMode = (commands[j] % 10000) / 1000;
+	int thirdParameterMode = (commands[j] % 100000) / 10000;
+
+	if (firstParameterMode == 0 && commands[j + 1] > (commands.size() - 1))
+	{
+		expandMemory(&commands, commands[j + 1]);
+	}
+	else if (firstParameterMode == 2 && (relativeBase + commands[j + 1]) > commands.size() - 1)
+	{
+		expandMemory(&commands, relativeBase + commands[j + 1]);
+	}
+	else if (firstParameterMode == 1 && j + 1 > commands.size() - 1)
+	{
+		expandMemory(&commands, j + 1);
+	}
+
+	if (secondParameterMode == 0 && commands[j + 2] > (commands.size() - 1))
+	{
+		expandMemory(&commands, commands[j + 2]);
+	}
+	else if (secondParameterMode == 2 && (relativeBase + commands[j + 2]) > commands.size() - 1)
+	{
+		expandMemory(&commands, relativeBase + commands[j + 2]);
+	}
+	else if (secondParameterMode == 1 && j + 2 > commands.size() - 1)
+	{
+		expandMemory(&commands, j + 1);
+	}
+
+	if (thirdParameterMode == 0 && commands[j + 3] > (commands.size() - 1))
+	{
+		expandMemory(&commands, commands[j + 3]);
+	}
+	else if (thirdParameterMode == 2 && (relativeBase + commands[j + 3]) > commands.size() - 1)
+	{
+		expandMemory(&commands, relativeBase + commands[j + 3]);
+	}
+	else if (thirdParameterMode == 1 && j + 3 > commands.size() - 1)
+	{
+		expandMemory(&commands, j + 3);
+	}
+
+	while (command != END_COMM)
+	{
+		switch (command)
+		{
+		case ADD_COMM:
+			if (thirdParameterMode == 0)
+			{
+				commands[commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) + (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == commands[j + 3]))
+					j = j + 4;
+			}
+			else //thirdParameterMode == 2
+			{
+				commands[relativeBase + commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) + (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == relativeBase + commands[j + 3]))
+					j = j + 4;
+			}
+			break;
+		case MUL_COMM:
+			if (thirdParameterMode == 0)
+			{
+				commands[commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) * (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == commands[j + 3]))
+					j = j + 4;
+			}
+			else //thirdParameterMode == 2
+			{
+				commands[relativeBase + commands[j + 3]] = (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) * (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+				if (!(j == relativeBase + commands[j + 3]))
+					j = j + 4;
+			}
+			break;
+		case INPUT_COMM:
+			(firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) = inputValue;
+			if (!(j == commands[j + 3]))
+				j = j + 2;
+			break;
+		case OUTPUT_COMM:
+			outputValues.push_back((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]));
+			//cout << "Output: " << outputValue << endl;
+			j = j + 2;
+			if (outputValues.size() == 3)
+			{
+				return;
+			}
+			break;
+		case JUMP_IF_TRUE:
+			if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) != 0)
+			{
+				j = (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+			}
+			else
+				j = j + 3;
+			break;
+		case JUMP_IF_FALSE:
+			if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) == 0)
+			{
+				j = (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]);
+			}
+			else
+				j = j + 3;
+			break;
+		case LESS_THAN:
+			if (thirdParameterMode == 0)
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) < (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[commands[j + 3]] = 1;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[commands[j + 3]] = 0;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			else //thirdParameterMode == 2
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) < (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[relativeBase + commands[j + 3]] = 1;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[relativeBase + commands[j + 3]] = 0;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			break;
+		case EQUALS:
+			if (thirdParameterMode == 0)
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) == (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[commands[j + 3]] = 1;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[commands[j + 3]] = 0;
+					if (!(j == commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			else //thirdParameterMode == 2
+			{
+				if ((firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]) == (secondParameterMode == 1 ? commands[j + 2] : secondParameterMode == 0 ? commands[commands[j + 2]] : commands[relativeBase + commands[j + 2]]))
+				{
+					commands[relativeBase + commands[j + 3]] = 1;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+				else
+				{
+					commands[relativeBase + commands[j + 3]] = 0;
+					if (!(j == relativeBase + commands[j + 3]))
+						j = j + 4;
+				}
+			}
+			break;
+		case ADJ_RELATIVE_BASE:
+			relativeBase += (firstParameterMode == 1 ? commands[j + 1] : firstParameterMode == 0 ? commands[commands[j + 1]] : commands[relativeBase + commands[j + 1]]);
+			j = j + 2;
+			break;
+		default:
+			std::cout << "UndefOpcode: " << command << std::endl;
+			++j;
+			break;
+		}
+
+		command = commands[j] % 100;
+		firstParameterMode = (commands[j] % 1000) / 100;
+		secondParameterMode = (commands[j] % 10000) / 1000;
+		thirdParameterMode = (commands[j] % 100000) / 10000;
+
+		if (firstParameterMode == 0 && commands[j + 1] > (commands.size() - 1))
+		{
+			expandMemory(&commands, commands[j + 1]);
+		}
+		else if (firstParameterMode == 2 && (relativeBase + commands[j + 1]) > commands.size() - 1)
+		{
+			expandMemory(&commands, relativeBase + commands[j + 1]);
+		}
+		else if (firstParameterMode == 1 && j + 1 > commands.size() - 1)
+		{
+			expandMemory(&commands, j + 1);
+		}
+
+		if (secondParameterMode == 0 && commands[j + 2] > (commands.size() - 1))
+		{
+			expandMemory(&commands, commands[j + 2]);
+		}
+		else if (secondParameterMode == 2 && (relativeBase + commands[j + 2]) > commands.size() - 1)
+		{
+			expandMemory(&commands, relativeBase + commands[j + 2]);
+		}
+		else if (secondParameterMode == 1 && j + 2 > commands.size() - 1)
+		{
+			expandMemory(&commands, j + 1);
+		}
+
+		if (thirdParameterMode == 0 && commands[j + 3] > (commands.size() - 1))
+		{
+			expandMemory(&commands, commands[j + 3]);
+		}
+		else if (thirdParameterMode == 2 && (relativeBase + commands[j + 3]) > commands.size() - 1)
+		{
+			expandMemory(&commands, relativeBase + commands[j + 3]);
+		}
+		else if (thirdParameterMode == 1 && j + 3 > commands.size() - 1)
+		{
+			expandMemory(&commands, j + 3);
+		}
+	}
+
+	computerStop = true;
+}
+
+int main(int argc, char* argv[])
+{
+	std::string buffer = "";
+	long long numberOfCommands = 0;
+
+	std::fstream input;
+	std::string fileLine;
+	input.open("input.txt", std::fstream::in);
+
+	while (std::getline(input, fileLine))
+	{
+		for (int i = 0; i < fileLine.size() + 1; ++i)
+		{
+			if (fileLine[i] == '-')
+			{
+				buffer.push_back(fileLine[i]);
+			}
+			else if (isdigit(fileLine[i]))
+			{
+				buffer.push_back(fileLine[i]);
+			}
+			else
+			{
+				commands.push_back(stoll(buffer));
+				buffer = "";
+				++numberOfCommands;
+			}
+		}
+	}
+
+	input.close();
+
+	std::vector <std::vector <int>> area;
+
+	const int widthArea = 100;
+	const int heightArea = 100;
+
+	for (long i = 0; i < heightArea; ++i)
+	{
+		std::vector <int> intBuffer;
+		for (long j = 0; j < widthArea; ++j)
+		{
+			intBuffer.push_back(NULL_VALUE);
+		}
+		area.push_back(intBuffer);
+	}
+
+	while (!computerStop)
+	{
+		IntCodeComputer();
+
+		if (computerStop)
+			break;
+
+		area[outputValues[1]][outputValues[0]] = outputValues[2];
+
+		outputValues.clear();
+	}
+
+	long countOfBlocks = 0;
+
+	int x = 0;
+	int y = 0;
+	system("cls");
+	while (area[y][x] != NULL_VALUE)
+	{
+		while (area[y][x] != NULL_VALUE)
+		{
+			switch (area[y][x])
+			{
+			case EMPTY:
+				printf("%c", 0);
+				break;
+			case WALL:
+				printf("%c", 178);
+				break;
+			case BLOCK:
+				printf("%c", 176);
+				++countOfBlocks;
+				break;
+			case HORIZONTAL_PADDLE:
+				printf("%c", 45);
+				break;
+			case BALL:
+				printf("%c", 111);
+				break;
+			}
+
+			++x;
+		}
+		printf("\n");
+
+		++y;
+		x = 0;
+	}
+
+	std::cout << std::endl << "countOfBlocks: " << countOfBlocks << std::endl;
+}
